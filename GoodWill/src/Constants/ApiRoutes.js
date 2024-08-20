4ed5ec4667a578d429bbc9bf86482d2b7c2acd33
OMKAR4064 <omkarpatil092001@gmail.com>
2024-08-20 17:45:37 +0530

    first commit

---
+++ GoodWill/src/Constants/ApiRoutes.js
@@ -0,0 +1,7 @@
+//For .NET
+export const BASE_URL = "http://localhost:5175/api";
+export const IMG_PATH = "http://localhost:5175/Resources";
+
+//For spring boot
+// export const BASE_URL = "http://localhost:8080/api";
+// export const IMG_PATH = "http://localhost:8080/Resources";
